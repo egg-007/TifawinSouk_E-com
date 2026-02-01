64533f5d4fe276bfe6179ae7ce90b5b3bfce570d
achraf abdelhakim essaloua <abdalhakimgo@gmail.com>
2026-02-01 21:01:33 +0100

    route for categories and admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategorieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('admin',AdminController::class);
+Route::resource('categories',CategorieController::class);
+Route::resource('categorie',CategorieController::class);
