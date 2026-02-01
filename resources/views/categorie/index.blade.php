fc2bb57d3c39b72c3ce1a3b032d3fe2316050ac8
achraf abdelhakim essaloua <abdalhakimgo@gmail.com>
2026-02-01 23:40:05 +0100

    update in edit and index code , solve problem of edit and delete

--- resources/views/categorie/index.blade.php
+++ resources/views/categorie/index.blade.php
@@ -38,12 +38,12 @@
                         </p>
 
                         <div class="flex gap-3">
-                            <a href="{{ route('categories.edit', $category) }}"
+                            <a href="{{ route('categories.edit', $category->id) }}"
                                class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition">
                                 Edit
                             </a>
 
-                            <form action="{{ route('categories.destroy', $category) }}"
+                            <form action="{{ route('categories.destroy', $category->id) }}"
                                   method="POST">
                                 @csrf
                                 @method('DELETE')
